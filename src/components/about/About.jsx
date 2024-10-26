459323f2fc6bd36438dfe9b2bbe0fef00a454fc7
Prajwal P <prajwalpr2@gmail.com>
2024-10-26 13:10:13 +0530

    changed about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,12 +5,12 @@ const about = () => {
     <div id="about" className="container m-auto mt-16">
       {/* heading */}
       <div data-aos="fade-up" className="relative mb-5">
-        <h3 className="text-3xl font-black text-gray-700 sm:text-2xl">PROFESSIONAL SUMMARY</h3>
+        <h3 className="text-3xl font-black text-gray-700 sm:text-2xl">Professional Summary</h3>
         <span className="h-[1.1px] right-0 absolute w-[100%] bg-gray-300 block"></span>
       </div>
       <div data-aos="fade-up" className="left flex-1 w-full">
   <p className="flex justify-content:normal text-gray-700 font-medium w-full sm:text-sm-center md:text-md-center text-justify">
-          Versatile and motivated Computer Science and Engineering graduate with a passion for crafting exceptional websites. Eager to leverage strong technical skills and a commitment to lifelong learning in a dynamic work environment. Proven adaptability to new technologies and deliver innovative solutions. Committed to excellence in web development and driven to contribute positively to challenging projects. Proficient in Data Structures and Algorithms, with a strong grasp of Object-Oriented Programming principles and applying these concepts to develop robust and efficient solutions.
+  Full-stack developer with 1+ year of experience specializing in Java, RESTEasy, SQL, JavaScript, and object-oriented programming (OOP) principles. Developed websites that solve real-world problems through effective frontend development and backend integration. Hands-on experience in building RESTful services and API design, as well as developing robust microservices for scalability within SDLC projects and Agile methodologies. Proficient in CI/CD pipeline implementation with Jenkins to automate deployments, consistently achieving 95% code quality as measured by SonarQube. Demonstrated expertise in frontend and backend development, ensuring seamless integration, responsive design, and optimal performance. Proven adaptability to new technologies and frameworks.
         </p>
       </div>
       
